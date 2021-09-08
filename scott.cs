d26b543a0fa23269b1d09c59480bbcd1e2b1d814
Scott Scherzer <Scott.Sch3rz3r@gmail.com>
2021-09-08 19:44:00 -0400

    Create scott.cs
    testing my push ability

---
+++ scott.cs
@@ -0,0 +1,22 @@
+using System;
+
+class scott {
+
+  /*
+  public student(String major, String school, int age){
+    this.major = major;
+    this.school = school;
+    this.age = age;
+  }
+  */
+
+  static public void Main(String[] args){
+
+		Console.WriteLine("Hello sendhelp!");
+
+    //student Scott = new student("Computer Science", "Marist College", 22);
+
+    //Console.WriteLine(Scott.age);
+
+	}
+}
