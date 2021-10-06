830470f606532bc09155f9b08d5dffaf7692d28c
Erin Alvarico <erinalvarico@gmail.com>
2021-10-06 18:07:57 -0400

    Design of Starting Screen
    Added:
    + bare bones atmosphere
    + title
    + buttons (not mapped yet)
    + animation for click animations

---
+++ [SENDHELP] ARI/Assets/Scripts/UIBehavior.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIBehavior : MonoBehaviour
+{
+
+    public void OnClickSwitchScene(string ScreenName)
+    {
+        SceneManager.LoadScene(ScreenName);
+    }
+
+    public void OnClickExitGame()
+    {
+        Application.Quit();
+    }
+
+}
---
+++ [SENDHELP] ARI/Assets/Scripts/dialog.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class dialog : MonoBehaviour
+{
+    public TextMeshProUGUI textDisplay;
+    public string[] sentences;
+    private int index;
+    public float typingSpeed;
+    public GameObject continueButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(Type());
+    }
+
+    void Update()
+    {
+        if(textDisplay.text == sentences[index]) 
+        {
+            continueButton.SetActive(true);
+        }
+    }
+
+    IEnumerator Type()
+    {
+        foreach (char letter in sentences[index].ToCharArray())
+        {
+            textDisplay.text += letter;
+            yield return new WaitForSeconds(typingSpeed);
+        }
+    }
+
+    public void NextSentence()
+    {
+        continueButton.SetActive(false);
+
+        if (index < sentences.Length - 1)
+        {
+            index++;
+            textDisplay.text = "";
+            StartCoroutine(Type());
+        } 
+        
+        else
+        {
+            textDisplay.text = "";
+            continueButton.SetActive(false);
+        }
+    }
+
+}
