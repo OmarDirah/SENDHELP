f1570fd388ddbb38834b887ffe3ce5283f147a11
OmarDirah <47092744+OmarDirah@users.noreply.github.com>
2021-10-13 12:44:55 -0400

    Basic Node Movement
    Node movement system working and camera working. Prefabs still being made.

---
+++ PointAndClick/Assets/Scripts/GameManager.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // Static ref to game manager 
+    public static GameManager ins;
+    // Keep track of current node
+    [HideInInspector]
+    public Node currentNode;
+    public CameraRig rig;
+
+
+
+    // instaniation of game manager 
+    void Awake() 
+    {
+
+        ins = this;
+    
+    
+    }
+     void Update()
+    {
+        // check for mouse input and if current node is a prop
+        if (Input.GetMouseButtonDown(1) && currentNode.GetComponent<Prop>() != null)
+        {
+            currentNode.GetComponent<Prop>().loc.Arrive();
+        }
+    }
+
+}
