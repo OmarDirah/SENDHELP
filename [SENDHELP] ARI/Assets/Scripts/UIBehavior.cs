0cf4b01219e572b80159c61493ad3da6933b2f10
Erin Alvarico <erinalvarico@gmail.com>
2021-10-13 17:58:42 -0400

    smol pushes
    previous push with UI but had too many files

--- [SENDHELP] ARI/Assets/Scripts/UIBehavior.cs
+++ [SENDHELP] ARI/Assets/Scripts/UIBehavior.cs
@@ -1,4 +1,7 @@
-﻿using System.Collections;
+﻿// [NAME] Erin Alvarico
+// [UI Behavior] List of methods/functiosn to use involving UI elements and how it interacts with the game world
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -6,10 +9,22 @@ using UnityEngine.SceneManagement;
 public class UIBehavior : MonoBehaviour
 {
 
+    // VARIABLES
+    //bool buttonSelect = false;
+
+
     public void OnClickSwitchScene(string ScreenName)
     {
         SceneManager.LoadScene(ScreenName);
     }
+
+    
+    /*public void OnClickChangeView(GameObject camera)
+    {
+        // Run CameraController Update Method to change to appropriate view
+        CameraController.Update();
+    }*/
+    
 
     public void OnClickExitGame()
     {
