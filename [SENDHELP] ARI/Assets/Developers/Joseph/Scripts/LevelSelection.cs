3d3578969001cdfde8ea5c003fcb4750795bef0f
Joseph McDonough <joseph.mcdonough1@marist.edu>
2021-11-10 02:40:05 -0500

    Can start at level selection (only can access tutorial). After interacting with terminal in tutoiral, sent back to level selection and level 1 is selectable. Can click and go to pre-quiz.

--- [SENDHELP] ARI/Assets/Developers/Joseph/Scripts/LevelSelection.cs
+++ [SENDHELP] ARI/Assets/Developers/Joseph/Scripts/LevelSelection.cs
@@ -6,16 +6,39 @@ using UnityEngine.UI;
 public class LevelSelection : MonoBehaviour
 {
     public Button[] lvlButtons;
+    public static List<int> levelListDone = new List<int>();
+    public int[] completeLevels = new int[3];
 
     void Start()
     {
-        int levelAt = PlayerPrefs.GetInt("levelAt", 2); 
+        int levelAt = PlayerPrefs.GetInt("levelAt", 2);
+
+        Debug.Log("COMPLETED LEVELS BEGINNING");
+        foreach (int i in completeLevels)
+        {
+            Debug.Log(i);
+        }
+        Debug.Log("COMPLETED LEVELS END");
+
+
+        Debug.Log("COMPLETED LEVELS BEGINNING V2");
+        foreach (int j in levelListDone)
+        {
+            Debug.Log(j);
+        }
+        Debug.Log("COMPLETED LEVELS END V2");
+
 
         for (int i = 0; i < lvlButtons.Length; i++)
         {
-            if (i + 2 > levelAt)
+            if ( (i + 2 > levelAt) && (!(levelListDone.Contains(i))) )
                 lvlButtons[i].interactable = false;
         }
     }
+
+    public void AddCompletedLevels(int levelToAdd)
+    {
+        levelListDone.Add(levelToAdd);
+    } 
 
 }
