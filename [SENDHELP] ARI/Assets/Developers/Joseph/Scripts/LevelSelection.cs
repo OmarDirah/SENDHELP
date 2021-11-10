c81db511db760bd992977f7edc17cd0e3f792e6d
Joseph McDonough <joseph.mcdonough1@marist.edu>
2021-11-10 01:39:03 -0500

    Buttons that are not accessible by default are unclickable (i.e. only the tutorial can be selected by the novice, proficient, and expert level screens can be shown

---
+++ [SENDHELP] ARI/Assets/Developers/Joseph/Scripts/LevelSelection.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelSelection : MonoBehaviour
+{
+    public Button[] lvlButtons;
+
+    void Start()
+    {
+        int levelAt = PlayerPrefs.GetInt("levelAt", 2); 
+
+        for (int i = 0; i < lvlButtons.Length; i++)
+        {
+            if (i + 2 > levelAt)
+                lvlButtons[i].interactable = false;
+        }
+    }
+
+}
